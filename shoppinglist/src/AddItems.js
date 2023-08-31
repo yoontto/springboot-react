30df8bc8980ae74137823b8d4033f42111fa793a
김윤서 <jedney69@gmail.com>
2023-08-31 10:40:57 +0900

    9장 : mui 컴포넌트 추가해서 shoppingList 만들기

--- shoppinglist/src/AddItems.js
+++ shoppinglist/src/AddItems.js
@@ -6,8 +6,12 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 
-function addItem(props) {
+function AddItem(props) {
     const [open, setOpen] = React.useState(false);
+    const [item, setItem] = React.useState({
+        product: '',
+        amount: ''
+    });
 
     const handleOpen = () => {
         setOpen(true);
@@ -17,9 +21,40 @@ function addItem(props) {
         setOpen(false);
     }
 
+    const handleChange = (e) => {
+        //스프레드 표기법
+        setItem({...item, [e.target.name]: e.target.value})
+    }
+
+    //props를 통해 addItem함수를 호출하고 항목 상태를 전달
+    const addItem = () => {
+        props.newItem(item);
+        setItem({product: '', amount: ''});
+        handleClose();
+    }  
+
     return(
-        <div></div>
+        <div>
+            <Button onClick={handleOpen}>
+                Add Item
+            </Button>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>New Item</DialogTitle>
+                <DialogContent>
+                    <TextField value={item.product} margin="dense" onChange={handleChange} name="product" label="Product" fullWidth />
+                    <TextField value={item.amount} margin="dense" onChange={handleChange} name="amount" label="Amount" fullWidth />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}> 
+                        Cancel
+                    </Button>
+                    <Button onClick={addItem}> 
+                        Add
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
     );
 }
 
-export default addItem;
+export default AddItem;
