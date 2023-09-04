eae350df075e0c8e67c3df1f319a23810bbde27b
김윤서 <jedney69@gmail.com>
2023-09-04 21:11:49 +0900

    10장 : 부트 준비하기

---
+++ carfront/src/App.js
@@ -0,0 +1,20 @@
+import AppBar from '@mui/material/AppBar';
+import ToolBar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AppBar>
+        <ToolBar>
+          <Typography>
+            Carshop
+          </Typography>
+        </ToolBar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default App;
